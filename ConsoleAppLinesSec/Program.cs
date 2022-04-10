1db258be8a740c3a165a3650c8b7d758c0477ded
Константин Пикатов <konstantinpikatov@gmail.com>
2022-04-10 10:21:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLinesSec/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLinesSec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inf = "информатика";
+            string infOne = "";
+            
+            
+            inf = inf.Remove(6, 1);    //индекс - кол-во 
+            inf = inf.Insert(6, "а ");   //индекс и замена
+            
+            infOne = inf.Substring(2, 9);
+
+            
+            Console.WriteLine(infOne);
+
+            Console.ReadKey();
+
+
+            //infOne = inf.Substring(2, 5);
+            //infTwo = inf.Substring(6, 9);
+            //string infSec = inf.Substring(7, 9);
+
+
+            //inf = inf.Remove(0, 2);
+
+            /*inf = inf.Remove(5, 6);
+            inf = inf.Insert(6, "а ");*/
+
+            /*inf = inf.Remove(10);
+            inf = inf.Remove(0, 2);
+            inf = inf.Remove(3, 3);*/
+
+            //Console.WriteLine(infOne + infTwo);
+
+            
+        }
+    }
+}
